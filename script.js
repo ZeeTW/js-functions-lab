a6576b801cab6c68adf355fc3b5743ac6c5b8154
ZeeTW <zainab.alafoo@hotmail.com>
2024-11-05 16:17:54 +0300

    Exercise 11git add .!

--- script.js
+++ script.js
@@ -207,3 +207,62 @@ const basicCalculator = (number1, number2, operation) => {
 }
 
 console.log('Exercise 9 Result:', basicCalculator(10, 5, 'subtract'))
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+const calculateGrade = (grade) => {
+  if (grade >= 90) {
+    return `A`
+  } else if (grade >= 80) {
+    return `B`
+  } else if (grade >= 70) {
+    return `C`
+  } else if (grade >= 60) {
+    return `D`
+  } else {
+    return `F`
+  }
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85))
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+const createUsername = (firstName, lastName) => {
+  let firstThree = ''
+  let lastThree = ''
+  let nameLength = firstName.length + lastName.length
+  for (let i = 0; i < 3; i++) {
+    firstThree += firstName[i]
+    lastThree += lastName[i]
+  }
+  return firstThree + lastThree + nameLength
+}
+console.log('Exercise 11 Result:', createUsername('Samantha', 'Green'))
